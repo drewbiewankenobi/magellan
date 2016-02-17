ba0fbf27cc9b186e63270354a4d1835e46b51f01
Evan Brauch <evan.brauch@gmail.com>
2016-02-17 15:29:54 -0700

    kinda works

--- app.js
+++ app.js
@@ -15,12 +15,17 @@ app.use(express.static(__dirname + '/public'));
 
 // Routes \\
 app.get('/', function(req, res){
-  res.sendText('')
+  res.sendFile('seville.html', {root : './public/'});
 });
 
+app.get('/:location', function(req, res){
+	console.log('/:location ->', req.params)
+	res.send('Magellan didn\'t visit ' + req.params.location)
+})
 // Creating Server and Listening for Connections \\
 var port = 3000
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
 })
+
