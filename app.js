f572b44c01741bacc744e83bc52aba8678c46c85
Student <student@RU-101.local>
2016-02-17 14:14:09 -0700

    first push

---
+++ app.js
@@ -0,0 +1,26 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+// Create Express App Object \\
+var app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+// Application Configuration \\
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+app.get('/', function(req, res){
+  res.sendText('')
+});
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
